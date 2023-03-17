fbc8d22017d671b3a296de51f266f8f215dd6db2
O-Liss <lssosc001@myuct.ac.za>
2023-03-17 15:20:09 +0200

    Q1.4
    init_switches()

--- src/main.c
+++ src/main.c
@@ -6,12 +6,14 @@
 void init_LEDs();
 void display_on_LCD(uint8_t num);
 void display_on_LEDs(uint8_t no);
+void init_switches();
 
 void main()
 {
 	//init_buttons();
 	init_LEDs();
 	init_LCD();
+	init_switches();
 
 	while(1)
 	{
@@ -42,4 +44,11 @@ void init_LEDs()
 void display_on_LEDs(uint8_t no)
 {
 	GPIOB->ODR = no;
+}
+
+void init_switches()
+{
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR1_0;
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR2_0;
 }
